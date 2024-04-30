dcdd44ec4cd7d8208444ed0e3225b8aa36213fcc
kinajin <kinajin22@gmail.com>
2024-04-30 17:03:21 +0900

    change setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { DataSource } from 'typeorm';
 // import { databaseConfig } from './config/database.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LottoStores } from './entity/LottoStores.entity';
+import { WinningInfos } from './entity/WinningInfos.entity';
 import { LottoStoreService } from './lotto-store.service';
 import { LottoStoreController } from './lotto-store.controller';
 import { ConfigModule } from '@nestjs/config';
@@ -11,7 +12,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env' }),
     TypeOrmModule.forRoot({
-      entities: [LottoStores],
+      entities: [LottoStores, WinningInfos],
       type: 'postgres',
       url: process.env.DATABASE_URL,
       ssl: {
