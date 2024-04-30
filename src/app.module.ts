f5743c1205ef9c6b8ad386c182366e80e91c993f
kinajin <kinajin22@gmail.com>
2024-04-30 15:59:03 +0900

    change setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env' }),
     TypeOrmModule.forRoot({
+      type: 'postgres',
       url: process.env.DATABASE_URL,
     }),
     TypeOrmModule.forFeature([LottoStores]),
